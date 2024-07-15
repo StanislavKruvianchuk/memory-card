4be66fd999abeedb7719707a5ea002362f108128
Stanislav <kruvianchuk@gmail.com>
2024-07-15 21:58:52 +0300

    add images

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ let cardOne, cardTwo;
 let disabledDeck = false;
 
 const flipCard = ({target: clickedCard}) => {
-    clickedCard.classList.add('flip')
+
+    if (disabledDeck || clickedCard === cardOne) return;
+
+    clickedCard.classList.add('flip');
+    
+    if (!cardOne) {
+        // First card clicked
+        cardOne = clickedCard;
+    } else {
+        // Second card clicked
+        cardTwo = clickedCard;
+        disabledDeck = true;
+
+        // Check if the cards match
+        let cardOneImg = cardOne.querySelector("img").src;
+        let cardTwoImg = cardTwo.querySelector("img").src;
+
+        if (cardOneImg === cardTwoImg) {
+            // Cards match
+            matched++;
+            cardOne.removeEventListener("click", flipCard);
+            cardTwo.removeEventListener("click", flipCard);
+            resetDeck();
+        } else {
+            // Cards do not match
+            setTimeout(() => {
+                cardOne.classList.remove("flip");
+                cardTwo.classList.remove("flip");
+                resetDeck();
+            }, 1000);
+        }
+    }
 }
-
-
 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
